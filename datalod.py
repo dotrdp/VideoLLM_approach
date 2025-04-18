7a73e9a31c4dcf9529b2e0cdc642f7fc7e913c91
dotrdp <rckydp1@gmail.com>
2025-04-17 18:21:14 -0600

    Added gitignore to account dataset

--- datalod.py
+++ datalod.py
@@ -1,13 +1,12 @@
 from datasets import load_dataset
-from torch.utils.data import DataLoader
 
-def LOADDATASET():
+
+def dat():
     # Load the dataset from Hugging Face
     # The dataset is streamed to save memory
     # and the DataLoader is set up for efficient data loading
     # with multiple workers and prefetching.
     
     # Load the dataset using streaming mode
-    ds = load_dataset('https://huggingface.co/datasets/lmms-lab/LLaVA-Video-178K', streaming=True)
-    dl = DataLoader(ds, num_workers=1, prefetch_factor=2, batch_size=1, pin_memory=True)
-    return dl
+    ds = load_dataset('lmms-lab/LLaVA-Video-178K','0_30_s_nextqa' , streaming=True)
+    return ds
