e21561b02564562f314f0287b01081758406bcba
dotrdp <rckydp1@gmail.com>
2025-04-20 03:16:22 -0600

    Started training

--- loader.py
+++ loader.py
@@ -1,5 +1,6 @@
 import json
 import mediapy as media
+from tqdm import tqdm
 def LoadActivityNetDataset():
     with open("data/activitynet/0_30_s_activitynetqa_oe_qa_processed.json", "r") as f:
         data = json.load(f)
@@ -12,6 +13,88 @@ def LoadNextQA_OE():
     with open("data/NextQA/0_30_s_nextqa_oe_qa_processed.json", "r") as f:
         data = json.load(f)
     return data
+def LoadNextQA_MC():
+    with open("data/NextQA/0_30_s_nextqa_mc_qa_processed.json", "r") as f:
+        data = json.load(f)
+    return data
+def LoadPerceptionDataset():
+    with open("data/perception/0_30_s_perceptiontest_mc_qa_processed.json", "r") as f:
+        data = json.load(f)
+    return data
+
+def LoadDatasets(rawdataYoutube, rawdataActivityNet, rawdataQA, rawdataQA2, rawdataperception):
+    training_data = []
+    labels_data = []
+    names = []
+    sources = []
+    for i in tqdm(range(0, len(rawdataYoutube)), desc="Loading Youtube Dataset"):
+        train = []
+        t,l = rawdataYoutube[i]["conversations"][0]["value"].replace("<image>",""), rawdataYoutube[i]["conversations"][1]["value"].replace("<image>","")
+        if len(t) >512 or len(l) >512:
+            continue
+        train.append(t)
+        train.append("data/youtube/"+rawdataYoutube[i]["video"])
+        training_data.append(train)
+        labels_data.append(
+        l
+        )
+        names.append(rawdataYoutube[i]["id"])
+        sources.append("youtube")
+    for i in tqdm(range(0, len(rawdataActivityNet)), desc="Loading ActivityNet open questions Dataset"):
+        train = []
+        t,l = rawdataActivityNet[i]["conversations"][0]["value"].replace("<image>",""), rawdataActivityNet[i]["conversations"][1]["value"].replace("<image>","")
+        if len(t) > 512 or len(l) >512:
+            continue
+        train.append(t)
+        train.append("data/activitynet/"+rawdataActivityNet[i]["video"])
+        training_data.append(train)
+        labels_data.append(
+        l
+        )
+        names.append(rawdataActivityNet[i]["id"])
+        sources.append("activitynet")
+    for i in tqdm(range(0, len(rawdataQA)), desc="Loading Next QO-A Dataset"):
+        train = []
+        t,l = rawdataQA[i]["conversations"][0]["value"].replace("<image>",""), rawdataQA[i]["conversations"][1]["value"].replace("<image>","")
+        if len(t) > 512 or len(l) >512:
+            continue
+        train.append(t)
+        train.append("data/NextQA/"+rawdataQA[i]["video"])
+        training_data.append(train)
+        labels_data.append(
+        l
+        )
+        names.append(rawdataQA[i]["id"].split("-")[1])
+        sources.append("NEXTQA")
+    
+    for i in tqdm(range(0, len(rawdataQA2)), desc="Loading Next Q-A Dataset"):
+        train = []
+        t,l = rawdataQA2[i]["conversations"][0]["value"].replace("<image>",""), rawdataQA2[i]["conversations"][1]["value"].replace("<image>","")
+        if len(t) > 512 or len(l) >512:
+            continue
+        train.append(t)
+        train.append("data/NextQA/"+rawdataQA2[i]["video"])
+        training_data.append(train)
+        labels_data.append(
+        l
+        )
+        names.append(rawdataQA2[i]["id"].split("-")[1])
+        sources.append("NEXTQA")
+    for i in tqdm(range(0, len(rawdataperception)), desc="Loading Perception Dataset"):
+        train = []
+        t,l = rawdataperception[i]["conversations"][0]["value"].replace("<image>",""), rawdataperception[i]["conversations"][1]["value"].replace("<image>","")
+        if len(t) > 512 or len(l) >512:
+            continue
+        train.append(t)
+        train.append("data/perception/"+rawdataperception[i]["video"])
+        training_data.append(train)
+        labels_data.append(
+        l
+        )
+        names.append(rawdataperception[i]["id"].replace("perceptiontest_",""))
+        sources.append("perception")
+        return training_data, labels_data, names, sources
+    
 
 def ShowVideo(video_path, dataset):
     if dataset == "activitynet":
